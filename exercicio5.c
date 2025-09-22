65975fb9b11b8a06909be595f97ac95435e486c8
Anne Karoline Goulart <annekgoulart@gmail.com>
2025-09-21 21:57:18 -0300

    Create exercicio5.c

---
+++ exercicio5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct ponto {
+    int x;
+    int y;
+    int z;
+};
+
+typedef struct ponto ponto;
+
+int main (){
+    ponto v1= {1,0,5};
+    ponto v2= {3,3,3};
+    ponto v3= {0,10,0};
+    
+    printf("Coordenada y de v1: %d\n", v1.y);
+    printf("Coordenada y de v2: %d\n", v2.y);
+    printf("Coordenada y de v3: %d\n", v3.y);
+    
+    v1.x+=10;
+    v2.x+=10;
+    v3.x+=10;
+    
+    printf("Coordenadas de v2: (%d, %d, %d)\n", v2.x, v2.y, v2.z);
+    return 0;
+}
